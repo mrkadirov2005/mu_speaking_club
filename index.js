c0c7a7f451b80e95a2c7ecf2ac4c8d73151c4c15
mrkadirov2005 <muzaffar571181@gmail.com>
2025-02-10 21:47:51 +0500

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const button=document.getElementById("button")
+button.addEventListener("click",()=>{
+    const name=document.getElementById("name").value;
+const surname=document.getElementById("surname").value;
+const email=document.getElementById("email").value;
+const number=document.getElementById("number").value;
+fetch("https://script.google.com/macros/s/AKfycbxXs48EoZl7Mj0ewD_A0aE8qS4oN5X_myXdJQEBEtkiaUXMKFKwAZ9DCmJBtKXhdt-3/exec",{
+    method:"POST",
+    header:"application/json",
+    mode:"no-cors",
+    body:JSON.stringify({name,surname,email,number})
+
+})
+})
